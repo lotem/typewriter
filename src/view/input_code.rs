a6a3f6a2c545da7cf7bff2bae56fb29d8163f126
居戎氏 <chen.sst@gmail.com>
2024-11-22 17:54:06 +0800

    修復: 在反應場外讀信號

--- src/view/input_code.rs
+++ src/view/input_code.rs
@@ -51,11 +51,11 @@ pub fn Rime練習題選單(
 ) -> impl IntoView {
     let 練習題選單的引用 = create_node_ref::<html::Select>();
     create_render_effect(move |_| {
+        let 選項序號 = 當選題號()
+            .and_then(|題號| 題號.try_into().ok())
+            .unwrap_or(-1);
         if let Some(輸入欄) = 練習題選單的引用() {
             let _不看結果 = 輸入欄.on_mount(move |輸入欄| {
-                let 選項序號 = 當選題號()
-                    .and_then(|題號| 題號.try_into().ok())
-                    .unwrap_or(-1);
                 輸入欄.set_selected_index(選項序號);
                 let _ = 輸入欄.focus();
             });
